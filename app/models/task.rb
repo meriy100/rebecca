2442b61ae1cc4e56b3340348b36d854aaf5fc882
meriy100 <ttattataa@gmail.com>
2016-05-24 19:36:29 +0900

    progress bar

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,16 @@ class Task < ActiveRecord::Base
   include CurrentUser
   acts_as_paranoid
 
+  def least_time
+    deadline_at - Time.zone.now
+  end
+
+  def full_time
+    deadline_at - created_at
+  end
+
+  def least_time_per
+    (least_time / full_time * 100).to_i
+  end
+
 end
