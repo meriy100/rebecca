d505aca023e2f5be156532fbe05eee966b1e23e6
meriy100 <ttattataa@gmail.com>
2016-06-04 16:46:40 +0900

    fix current_user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,24 +1,20 @@
 class ApiController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_user
   before_filter :authenticated
 
   def authenticated
     if session[:user_id]
       begin
-        @user = User.find(session[:user_id])
-        User.current_user = @user
+        @current_user = User.find(session[:user_id])
+        User.current_user = @current_user
       rescue ActiveRecord::RecordNotFound
         reset_session
       end
     end
     redirect_to login_path unless @current_user
   end
-  def set_user
-    @current_user = User.find session[:user_id]
-  end
 
   def current_user
-    @current_user
+    User.current_user
   end
 end
