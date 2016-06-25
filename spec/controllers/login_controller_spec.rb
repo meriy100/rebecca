cb61cd5f7e037022d1f787e8585a2e54db1f6b99
meriy100 <ttattataa@gmail.com>
2016-06-25 18:28:30 +0900

    login spec

---
+++ spec/controllers/login_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+
+RSpec.describe LoginController, type: :controller do
+
+  let(:valid_attributes) {
+  }
+
+  let(:invalid_attributes) {
+  }
+  let(:valid_session) { {} }
+
+  describe "GET #index" do
+    it "render insec" do
+      get :index, {}, valid_session
+      expect(response).to render_template :index
+    end
+  end
+
+  describe "POST #login" do
+    let(:user) { create(:user) }
+    context "valid_params" do
+      it "when email" do
+        post :login, { email_or_name: user.email, password: "testpass" }
+        expect(response).to redirect_to top_path
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it "when name" do
+        post :login, { email_or_name: user.name, password: "testpass" }
+        expect(response).to redirect_to top_path
+        expect(session[:user_id]).to eq(user.id)
+      end
+    end
+    context "invalid_params" do
+      it "when email" do
+        post :login, { email_or_name: user.email + "a", password: "testpass" }
+        expect(response).to render_template :index
+        expect(session[:user_id]).to be_falsey
+      end
+
+      it "when name" do
+        post :login, { email_or_name: user.name + "a", password: "testpass" }
+        expect(response).to render_template :index
+        expect(session[:user_id]).to be_falsey
+      end
+
+      it "password" do
+        post :login, { email_or_name: user.name, password: "invalidpass" }
+        expect(response).to render_template :index
+        expect(session[:user_id]).to be_falsey
+      end
+    end
+  end
+
+end
