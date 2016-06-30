34595e43723311185f626763e4ae9947e282af38
meriy100 <ttattataa@gmail.com>
2016-07-01 00:10:46 +0900

    タブとセレクトボックス

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,6 @@
 class SettingsController < ApplicationController
   def show
-    current_user.setting
+    @setting = current_user.setting || Setting.new(start_week_day_id: 2)
     @task = Task.new
     @search = Task.search(params[:q])
     @filter = { title: "タスク一覧", path: tasks_path }
