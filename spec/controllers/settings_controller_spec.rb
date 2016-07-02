aee21fe22b81d15cd3600eee499e60161911ff57
meriy100 <ttattataa@gmail.com>
2016-07-02 19:26:46 +0900

    update spec for setting

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -13,4 +13,21 @@ RSpec.describe SettingsController, type: :controller do
       end
     end
   end
+  describe "PATCH update" do
+    let(:setting) { create(:setting) }
+    context "start_week_day" do
+      it "is changed" do
+        patch :update, { setting: { start_week_day_id: 2 } }, valid_session
+        setting.reload
+        expect(setting.start_week_day_id).to eq 2
+      end
+    end
+    context "time_format" do
+      it "is changed" do
+        patch :update, { setting: { time_format_id: 2 } }, valid_session
+        setting.reload
+        expect(setting.time_format_id).to eq 2
+      end
+    end
+  end
 end
--- spec/factories/settings.rb
+++ spec/factories/settings.rb
@@ -2,8 +2,14 @@ FactoryGirl.define do
   factory :setting do
     start_week_day_id 1
     time_format_id 1
-    factory :sunday do
+    factory :sunday_setting do
       start_week_day_id 1
+    end
+    factory :monday_setting do
+      start_week_day_id 1
+    end
+    factory :other_format_setting do
+      time_format_id 1
     end
   end
 end
