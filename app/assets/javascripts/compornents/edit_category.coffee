2738f10ed5e0f89d60ef79f34426e48ee3b38534
takenoko <tk.hrsk79@gmail.com>
2016-07-14 17:06:31 +0900

    dropdownで選択したやつのidをhiddenformにシューツ

--- app/assets/javascripts/compornents/edit_category.coffee
+++ app/assets/javascripts/compornents/edit_category.coffee
@@ -1,3 +1,8 @@
 $(document).ready ->
   $(document).on "click", ".new-category", ->
     $(".new-category-form").show()
+  $(document).on "click", ".icon-cand", ->
+    console.log($(this).find(".fa").attr("value"))
+    $("#category_icon_id").val($(this).find("i").attr("value"))
+  $(document).on "click", ".color-cand", ->
+    $("#category_color_id").val($(this).find("font").attr("value"))
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicatinController
   before_action :set_category, only: [:update, :destroy]
 
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    render nothing: true
+  end
+
   def set_category
     @category = Category.on_user.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :row_order, :user_id, :color_id, :sync_token, :icon_id, :created_at, :updated_at)
   end
 end
